1ec5fe1251c2ff6a99fc845215eacb8cc7564ecb
ChoiHyunSan <chsan626@gmail.com>
2024-12-24 23:36:53 +0900

    first commit

---
+++ backend/TimeMate-Project/src/main/java/com/ll/timemateproject/TimeMateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ll.timemateproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeMateProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeMateProjectApplication.class, args);
+    }
+
+}
---
+++ backend/TimeMate-Project/src/test/java/com/ll/timemateproject/TimeMateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.timemateproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeMateProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
