0dfad76fca7cd793ba2e0935d16b58edbbe89689
ChoiHyunSan <chsan626@gmail.com>
2024-12-25 23:58:36 +0900

    AuthenticationException 예외처리 추가

--- backend/TimeMate-Project/src/main/java/com/ll/timemateproject/global/exception/GlobalExceptionHandler.java
+++ backend/TimeMate-Project/src/main/java/com/ll/timemateproject/global/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.ll.timemateproject.global.exception;
 import com.ll.timemateproject.api.v1.dto.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,5 +16,10 @@ public class GlobalExceptionHandler {
     public Result<Void> handleResourceNotFoundException(BadCredentialsException e) {
         log.error("인증 관련 입력이 잘못되었습니다. : {}", e.getMessage(), e);
         return Result.error(401, "입력이 잘못되었습니다.");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public Result<Void> handleAuthenticationException(AuthenticationException e){
+        return Result.error(401, "로그인 정보가 잘못되었습니다.");
     }
 }
