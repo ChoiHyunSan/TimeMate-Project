691694e7dd0b9e992dcef85189bc87687861bc5c
ChoiHyunSan <chsan626@gmail.com>
2024-12-25 20:19:09 +0900

    Result 객체 설계

--- backend/TimeMate-Project/src/main/java/com/ll/timemateproject/TimeMateProjectApplication.java
+++ backend/TimeMate-Project/src/main/java/com/ll/timemateproject/TimeMateProjectApplication.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class TimeMateProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TimeMateProjectApplication.class, args);
     }
-
 }
